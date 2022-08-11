12a456ac8644b387c5f1e959d013e196c1ef3cae
Maury Marmolejo <maury.mdin@gmail.com>
2022-08-11 15:49:36 -0500

    FirstFunc

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Maury on 11/08/22.
+//
+
+import UIKit
+import FirstLibrery
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let firstClass = FirstClass()
+    }
+
+
+}
+
---
+++ Sources/FirstClass/FirstClass.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maury on 11/08/22.
+//
+
+import Foundation
+
+public class FirstClass {
+    
+    public init() {
+        print("From Librery")
+    }
+}
